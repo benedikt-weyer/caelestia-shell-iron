837a4eb406dda957a417492c71ab24be3ab5168e
Benedikt Weyer <bw.development@pm.me>
2026-09-07 13:39:12 +0200

    feat: expose dock config to QML
    Signed-off-by: Benedikt Weyer <bw.development@pm.me>

--- plugin/src/Caelestia/Config/configattached.hpp
+++ plugin/src/Caelestia/Config/configattached.hpp
@@ -20,6 +20,7 @@ class Config : public QQuickAttachedPropertyPropagator, public QQmlParserStatus 
     Q_PROPERTY(const caelestia::config::BarConfig* bar READ bar NOTIFY sourceChanged)
     Q_PROPERTY(const caelestia::config::BorderConfig* border READ border NOTIFY sourceChanged)
     Q_PROPERTY(const caelestia::config::DashboardConfig* dashboard READ dashboard NOTIFY sourceChanged)
+    Q_PROPERTY(const caelestia::config::DockConfig* dock READ dock NOTIFY sourceChanged)
     Q_PROPERTY(const caelestia::config::LauncherConfig* launcher READ launcher NOTIFY sourceChanged)
     Q_PROPERTY(const caelestia::config::LockConfig* lock READ lock NOTIFY sourceChanged)
     Q_PROPERTY(const caelestia::config::NexusConfig* nexus READ nexus NOTIFY sourceChanged)
@@ -43,6 +44,7 @@ public:
     [[nodiscard]] const BarConfig* bar() const;
     [[nodiscard]] const BorderConfig* border() const;
     [[nodiscard]] const DashboardConfig* dashboard() const;
+    [[nodiscard]] const DockConfig* dock() const;
     [[nodiscard]] const LauncherConfig* launcher() const;
     [[nodiscard]] const LockConfig* lock() const;
     [[nodiscard]] const NexusConfig* nexus() const;
